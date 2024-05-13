fc4cf92f75b601794d31118fbaf93fa9c2766a35
Laurence Lewis <laurence.r.lewis@gmail.com>
2024-05-13 14:22:34 +1000

    installation of lit and dependancies

---
+++ accessible-webcomponents/src/accessible-webcomponents.ts
@@ -0,0 +1,6 @@
+import { AccessibleWebcomponents } from './AccessibleWebcomponents.js';
+
+window.customElements.define(
+  'accessible-webcomponents',
+  AccessibleWebcomponents
+);
---
+++ accessible-webcomponents/src/index.ts
@@ -0,0 +1,1 @@
+export { AccessibleWebcomponents } from './AccessibleWebcomponents.js';
